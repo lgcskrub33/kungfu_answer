751291581c0ee9a8b96f7116eea0c2e334f2dfed
cskrub33 <cskrub33@naver.com>
2020-06-04 21:03:53 +0900

    runnable add

---
+++ src/Thread2/ThreadExam2.java
@@ -0,0 +1,22 @@
+package Thread2;
+
+import Thread1.MyThread1;
+
+public class ThreadExam2 {
+
+    public static void main(String[] args) {
+        MyThread2 t1 = new MyThread2("*");
+        MyThread2 t2 = new MyThread2("-");
+
+        Thread thread1 = new Thread(t1);
+        Thread thread2 = new Thread(t2);
+
+        thread1.start();
+        thread2.start();
+
+        System.out.println("main end!!!!");
+
+
+
+    }
+}
